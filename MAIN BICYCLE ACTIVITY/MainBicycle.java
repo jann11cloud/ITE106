333512eb608a838ad437ba8c2495727a52d64c51
jann11cloud <jannmariebuena@gmail.com>
2024-10-18 13:51:53 +0800

    Delete MAIN BICYCLE ACTIVITY/MainBicycle.java

--- MAIN BICYCLE ACTIVITY/MainBicycle.java
+++
@@ -1,9 +0,0 @@
-public class MainBicycle {
-	public static void main (String[] args) {
-		
-		Bicycle sportsBicycle = new Bicycle();
-		
-		System.out.println(sportsBicycle.gear);
-		sportsBicycle.braking();
-	}
-}
